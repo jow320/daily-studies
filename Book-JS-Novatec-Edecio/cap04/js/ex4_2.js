a28d27b0071cba5d9cfdea7f559a2f89060a7c72
Victor Estevan <jow_d3@hotmail.com>
2024-08-16 19:34:36 -0300

    endbook160824

---
+++ Book-JS-Novatec-Edecio/cap04/js/ex4_2.js
@@ -0,0 +1,24 @@
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = frm.inNome.value;
+  const masculino = frm.inMasculino.checked;
+  const altura = Number(frm.inAltura.value);
+
+  const peso = masculino ? 22 * Math.pow(altura, 2) : 21 * Math.pow(altura, 2);
+
+  //   if (masculino) {
+  //     peso = 22 * Math.pow(altura, 2);
+  //   } else {
+  //     peso = 21 * Math.pow(altura, 2);
+  //   }
+
+  resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(1)} kg`;
+});
+
+frm.addEventListener("reset", () => {
+  resp.innerText = "";
+});
