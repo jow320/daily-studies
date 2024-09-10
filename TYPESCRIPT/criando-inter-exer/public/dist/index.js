cc326a513b30cb61ce0a13892e62193d1193c78f
Victor Estevan <jow_d3@hotmail.com>
2024-09-10 18:08:31 -0300

    10092024

---
+++ TYPESCRIPT/criando-inter-exer/public/dist/index.js
@@ -0,0 +1,40 @@
+const users = [];
+async function fetchUser(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const user = await response.json();
+    if (user.message) {
+        alert("Usuário não encontrado!");
+    }
+    else {
+        users.push(user);
+        alert(`O usuário ${user.login} foi salvo.\n` +
+            `\nid: ${user.id}` +
+            `\nlogin: ${user.login}` +
+            `\nNome: ${user.name}` +
+            `\nBio: ${user.bio}` +
+            `\nRepositórios públicos: ${user.public_repos}`);
+    }
+}
+async function showUser(username) {
+    const user = users.find((user) => user.login === username);
+    if (typeof user === "undefined") {
+        alert("Usuário não encontrado!");
+    }
+    else {
+        const response = await fetch(user.repos_url);
+        const repos = await response.json();
+        let message = `id: ${user.id}\n` +
+            `\nlogin: ${user.login}` +
+            `\nNome: ${user.name}` +
+            `\nBio: ${user.bio}` +
+            `\nRepositórios públicos: ${user.public_repos}`;
+        repos.forEach((repo) => {
+            message +=
+                `\nNome: ${repo.name}` +
+                    `\nDescrição: ${repo.description}` +
+                    `\nEstrelas: ${repo.stargazers_count}` +
+                    `\nÉ um fork: ${repo.fork ? "Sim" : "Não"}\n`;
+        });
+        alert(message);
+    }
+}
---
+++ TYPESCRIPT/criando-inter-exer/src/index.ts
@@ -0,0 +1,65 @@
+interface GithubUserResponse {
+  id: number;
+  login: string;
+  name: string;
+  bio: string;
+  public_repos: number;
+  repos_url: string;
+  message?: "Not Found";
+}
+
+interface GithubRepoResponse {
+  name: string;
+  description: string;
+  fork: boolean;
+  stargazers_count: number;
+}
+
+const users: GithubUserResponse[] = [];
+
+async function fetchUser(username: string) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const user: GithubUserResponse = await response.json();
+
+  if (user.message) {
+    alert("Usuário não encontrado!");
+  } else {
+    users.push(user);
+
+    alert(
+      `O usuário ${user.login} foi salvo.\n` +
+        `\nid: ${user.id}` +
+        `\nlogin: ${user.login}` +
+        `\nNome: ${user.name}` +
+        `\nBio: ${user.bio}` +
+        `\nRepositórios públicos: ${user.public_repos}`
+    );
+  }
+}
+async function showUser(username: string) {
+  const user = users.find((user) => user.login === username);
+
+  if (typeof user === "undefined") {
+    alert("Usuário não encontrado!");
+  } else {
+    const response = await fetch(user.repos_url);
+    const repos: GithubRepoResponse[] = await response.json();
+
+    let message =
+      `id: ${user.id}\n` +
+      `\nlogin: ${user.login}` +
+      `\nNome: ${user.name}` +
+      `\nBio: ${user.bio}` +
+      `\nRepositórios públicos: ${user.public_repos}`;
+
+    repos.forEach((repo) => {
+      message +=
+        `\nNome: ${repo.name}` +
+        `\nDescrição: ${repo.description}` +
+        `\nEstrelas: ${repo.stargazers_count}` +
+        `\nÉ um fork: ${repo.fork ? "Sim" : "Não"}\n`;
+    });
+
+    alert(message);
+  }
+}
