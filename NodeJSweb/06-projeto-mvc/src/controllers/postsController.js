d3470ca60f6acbe5dd72985e4636fa0eaccf9ffa
jow-ubuntu <jow_d3@hotmail.com>
2024-10-11 15:24:33 -0300

    111024

---
+++ NodeJSweb/06-projeto-mvc/src/controllers/postsController.js
@@ -0,0 +1,7 @@
+const postsController = {
+  index: (req, res) => {
+    res.send("Hello World!");
+  },
+};
+
+module.exports = postsController;
--- NodeJSweb/06-projeto-mvc/src/routes.js
+++ NodeJSweb/06-projeto-mvc/src/routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const postsController = require("./controllers/postsController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+router.get("/", postsController.index);
 
 module.exports = router;
