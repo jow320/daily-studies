f7e0b91fe112520a071de8e0d7f4e2c16db300e6
Victor Estevan <jow_d3@hotmail.com>
2026-02-02 14:15:25 -0300

    Commit of if-else study C

---
+++ C/Faculdade-ADS/estruturadecisaosimples.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+int numero1, numero2;
+int idade = 20;
+
+numero1 = 10;
+
+/*if (numero1 > numero2)
+{
+    printf("Número 1 é maior que o número 2");
+}
+
+
+} */
+
+if (idade >= 18)
+{
+    printf("Você é maior de idade");
+}
+
+
+
+
+}
