9a7ba3ca391152591b78c16b75c105c915d407b6
Victor Estevan <jow_d3@hotmail.com>
2026-02-04 20:47:30 -0300

    Commit Logic operators and chained structures c

---
+++ C/Faculdade-ADS/estruturasencadeadas.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+
+int main(){
+
+    int idade;
+
+    printf("Qual a idade?");
+    scanf( "%u", &idade);
+
+    if (idade < 12){
+        printf("Você é uma criança!\n");
+    } else if (idade >= 12 && idade < 18){
+        printf("Você é um adolescente!\n");
+    } else if (idade >= 18 && idade < 60)
+    {
+        printf("Você é um adulto!\n");
+    } else
+    {
+        printf("Você é um idoso\n");
+    }
+    
+    
+
+}
